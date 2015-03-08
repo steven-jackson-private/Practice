d450cc17bded64d52a897a241d7016759073ccdd
Steven <steven@roimedia.co.za>
2015-03-08 20:15:41 +0200

    Basic One page
    CSS3 Transition and Transform effects

---
+++ Nephritis/index.php
@@ -0,0 +1,202 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Nephritis</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+            <![endif]-->
+
+            <!-- Add your site or application content here -->
+
+            <!-- Start Header -->
+            <header>
+                <div class="nav-wrapper row">  
+                    <!-- Logo -->
+                    <div class="logo col-md-2 col-sm-3 col-xs-12 .navbar-collapse">  
+                        <div class="logo-wrapper">   
+                            Design<span>&ampCode</span>
+                        </div>
+                    </div>
+                    <!-- Nav -->
+                    <nav class="col-md-offset-6 col-md-4 collapsed navbar-collapse">
+
+                        <ul >
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Work</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+
+
+                    </nav>
+
+                </div>
+                <!-- Jumbotron -->
+                <div class="row">   
+                    <div class="jumbotron text-center"> 
+                        <h1>LET'S WORK TOGETHER</h1>
+
+                        <a href="#">   See my work</a>
+                    </div>
+                </div>
+
+            </header><!-- End Header -->
+            
+
+            <div id="content">
+
+                <div class="row"><!-- Sample Text  -->
+
+                    <section class="about-us text-center">
+                        <h3>We love make things amazing and simple</h3>
+                        <img src="img/vector-img.png" alt="">
+                        <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.</p>
+
+                    </section>
+                </div><!-- End -->
+
+                <div class="row">  <!-- Our Work  -->
+
+                    <section class="our-work text-center">
+                        <p>Work</p>
+
+                        <div class="row" id="portfolio">
+                            <div class="container">
+
+
+                             <!-- Box Overlay -->
+                                <div class="box" style="background:url(img/portfolio-img-home-1.png) no-repeat;">
+
+                                  <div class="overlay">
+                                     Would you like to know more?</br></br>
+
+                                     <a href="#">click here</a>
+                                 </div>
+                             </div>
+                            <!-- Box Overlay End-->
+
+
+                             <div class="box-flip">
+
+                              <div class="front" ></div>
+                             <div class="back">CSS 3 Box flip</div>
+
+                            </div>
+
+
+
+                            <div class="box rotate" style="background:url(img/portfolio-img-home-3.png) no-repeat;">
+
+                              <div class="overlay">
+                                 <a href="#"> Are you dizzy yet?
+                                 </div></a>
+                             </div>
+
+
+                             <div class="box" style="background:url(img/portfolio-img-home-4.png) no-repeat;">
+
+                              <div class="overlay-side">
+                                  <a href="#"> hello
+                                  </div></a>
+                              </div>
+
+                          </div> <!-- End Container -->
+
+
+
+
+                          <!-- CSS 3 Button -->
+                          <div class="clearfix"></div>
+                          <div class="wrapper">   
+                            <a href="#">See more</a>
+                        </div>
+
+
+                    </section>
+                </div> <!-- End -->
+
+
+
+                <div class="row"> <!-- Worked with -->
+
+                    <section class="worked-with">
+
+                        <h3 class="text-center">We've Worked with</h3>
+                        <div class="row ">   
+                            <div class="col-md-2 col-md-offset-2  col-sm-3 "> <img src="img/worked-with-img-1.png" alt="">  </div>
+
+                            <div class="col-md-2 col-sm-3"> <img src="img/worked-with-img-2.png" alt="">  </div>
+
+                            <div class="col-md-2  col-sm-3"> <img src="img/worked-with-img-3.png" alt="">  </div>
+
+                            <div class="col-md-2  col-sm-3 google"> <img src="img/worked-with-img-4.png" alt="">  </div>
+                            <div class="col-md-offset-2  col-sm-3 col-xs-12"> </div>
+
+                        </div>
+                    </section>
+                </div><!-- End -->
+
+
+
+
+            </div> <!-- Content div end -->
+
+
+
+
+
+
+
+            <footer> <!-- Start footer -->
+                <div class="row"> 
+
+                    <div class="social-icons col-md-3">
+                        <!-- <span class="social-facebook"></span> --> <img src="img/social-icon-f.png" alt=""> Facebook
+                        <img src="img/social-icon-f.png" alt=""> Twitter
+                        <img src="img/social-icon-f.png" alt=""> Dribbble
+
+
+                    </div>
+
+                    <div class="copyright col-md-4 col-md-offset-5 col-xs-12">
+                        &copy Design &amp Code 2013.  All rights reserved
+                        <span class="social-facebook"></span>
+                    </div>
+
+                </div>
+            </footer>  <!-- End Footer -->
+
+
+            <!-- Scripts -->
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script
+                <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+                <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+                <script src="js/plugins.js"></script>
+                <script src="js/main.js"></script>
+
+                <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+                <script>
+                    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+                        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+                    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+                    e.src='//www.google-analytics.com/analytics.js';
+                    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+                    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+                </script>
+            </body>
+            </html>
