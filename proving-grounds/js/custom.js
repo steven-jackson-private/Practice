53388f60a922e6f968436f484ab0fd2019b3116d
Steven <steven@roimedia.co.za>
2015-03-09 19:19:28 +0200

    Hidden content added
    Simple Jquery animation for hide/show hidden content

--- proving-grounds/js/custom.js
+++ proving-grounds/js/custom.js
@@ -10,6 +10,29 @@ $(document).ready(function(){
 	// $(".product-2").hover(function(){
  	//        $(this).hide();
  	//    });
+ 	
+
+
+
+ 	//Animate to 500px
+	$(".product-2").mouseenter(function(){
+ 	$(this).animate({
+ 		height: "500px",
+ 		width: "500px"},
+ 		600, function() {
+ 		
+ 	}),
+$(".product-2").mouseleave(function(){
+ 	$(this).animate({
+ 		height: "200px",
+ 		width: "250px"},
+ 		600, function() {
+ 		/* stuff to do after animation is complete */
+ 	});
+ 	
+    });
+ });
+	//Animate to 500px end
 
 
 	//Mouse enter and exit state
@@ -19,6 +42,8 @@ $(document).ready(function(){
 	$(".product-3").mouseleave(function(){
         $(this).show(800);
     });
+//Mouse enter and exit state end
+
 
 //Mouse enter and exit state
 //Better to do overlay in Css
@@ -29,6 +54,7 @@ $(document).ready(function(){
  //        $('.overlay').css('opacity', '0').hide(800);
  //    });
 
+//Animate to 500px
 $(".product-4").mouseenter(function(){
  	$(this).animate({
  		height: "220px",
@@ -46,30 +72,32 @@ $(".product-4").mouseleave(function(){
 
   });
   });
-
-//Animate to 500px
-	$(".product-2").mouseenter(function(){
- 	$(this).animate({
- 		height: "500px",
- 		width: "500px"},
- 		600, function() {
- 		
- 	}),
-$(".product-2").mouseleave(function(){
- 	$(this).animate({
- 		height: "200px",
- 		width: "250px"},
- 		600, function() {
- 		/* stuff to do after animation is complete */
- 	});
- 	
-
-
-
+//Animate to 500px end
+ $(".close-hidden-content").hide();
+ 
+$(".key").click(function(){
+    $('.hidden-content').animate({
+        
+        height: "500px",
+        width: "250px"},
+        600, function() {
+         $(".close-hidden-content").show();
+    }),
+$(".close-hidden-content").click(function(){
+    $(".hidden-content").animate({
+        height: "20px",
+        width: "250px"},
+        600, function() {
+        $(".close-hidden-content").hide();
     });
- });
+
+  });
+  });
 
  	
+//     $(".key").click(function(){
+// $(".key").hide();
+//     });
 
 
 
